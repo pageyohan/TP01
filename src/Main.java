324c9f070fc9eaf6d6dee37106563a845a6f50ec
pageyohan <page.yohan@etu.educ-valadon-limoges.fr>
2023-01-17 15:39:55 +0100

    ajout de la classe client

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+// EXERCICE 2 : Total de 3 prix
+        //float prix1, prix2, prix3, total;
+        //prix1 = 11.5f;
+        //prix2 = 8.7f;
+        //prix3 = 4;
+        //total = prix1+prix2+prix3;
+        //System.out.println(total);
+
+//Exercice 3: Variables et transtypage
+        //double nombre = 3.14;
+        //System.out.println(nombre);
+        //int entier;
+        //entier = (int) nombre;
+        // System.out.println(entier);
+
+//Exercice 4 : String et concaténation
+
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Saisissez votre prénom");
+        //String prenom = sc.next();
+        //System.out.println(prenom);
+
+//Exercice 5 : Saisie de nombres
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choisissez un nombre");
+        int nombre = Integer.parseInt(sc.next());
+        System.out.println(nombre);
+    }
+}
